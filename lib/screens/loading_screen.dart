67852ca2ebcbc450fcdcea4ba34d91edc4dac2ab
Aleksandra Petrova <petrowa.alex@yandex.com>
2022-12-02 23:56:38 +0300

    feat: added loading screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_weather_app_by_leksidev/screens/location_screen.dart';
 import 'package:flutter_weather_app_by_leksidev/services/location.dart';
 import 'package:flutter_weather_app_by_leksidev/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = 'cf56b3288aea7bdbb6432d42b2eaef2f';
 const authority = 'api.openweathermap.org';
@@ -16,8 +18,8 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  double? longetude;
-  double? latitude;
+  late double longetude;
+  late double latitude;
 
   @override
   void initState() {
@@ -47,10 +49,28 @@ class _LoadingScreenState extends State<LoadingScreen> {
     ));
 
     var weatherData = await networkHelper.getData();
+
+    // ignore: use_build_context_synchronously
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return const LocationScreen();
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+          duration: Duration(milliseconds: 4000),
+        ),
+      ),
+    );
   }
 }
